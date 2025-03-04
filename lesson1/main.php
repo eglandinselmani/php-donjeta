278ea687339f420de320931fc4710e8c3ac9dc78
eglandinselmani <eglandinselmani1@gmail.com>
2025-03-04 20:44:28 +0100

    lesson1

---
+++ lesson1/main.php
@@ -0,0 +1,28 @@
+<?php
+
+$school = "digital school";
+
+    $numri1 = 20;
+    $numri2 =1200;
+
+    echo $numri1 + $numri2;
+    echo "<br>";
+    echo $numri1 * $numri2;
+    echo "<br>";
+    echo $numri1 / $numri2;
+
+    echo "<br>";
+
+
+
+
+
+
+    $numri3 = 10;
+    $numri4 = 2;
+
+    $numri3 % $numri4
+    echo "<br>"
+
+    echo "i love $school";
+?>
