2d6d8ec78af918d99c5fe5b0e39b5f1cbbb36da6
eglandinselmani <eglandinselmani1@gmail.com>
2025-04-17 20:47:29 +0200

    APET ME NDREQ

---
+++ lesson12/dashboard.php
@@ -0,0 +1,47 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        
+    include_once("eglandin.php");
+    $sql="select * from user";
+    $getUsers=$conn->prepare($sql);
+    $getUser->execute();
+    $users=$getUsers->fetchAll();
+    
+    
+    
+    ?>    
+    <table>
+        <thead>
+        <th>ID</th>
+        <th>name</th>
+        <th>surname</th>
+        <th>email</th>
+        </thead>
+
+        <tbody>
+            <?php 
+            foreach($users as $user) {
+            ?>
+            <tr>
+                <td><?= $user['id']?></td>
+                <td><?= $user['name']?></td>
+                <td><?= $user['surname']?></td>
+                <td><?= $user['email']?></td>
+                <td><?="<a href='delete.php?id=$user[id]'>Delete</a>| <a href='edit.php'?id=$user[id]'> Update </a>"?></td>
+            </tr>
+
+
+            <?php } ?>
+        </tbody>
+        
+
+    </table>
+    <a href="index.php">go to indekss</a>
+</body>
+</html>
