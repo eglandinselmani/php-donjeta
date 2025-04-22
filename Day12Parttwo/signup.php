2541c401219a720c77e16ff066fe65ebbdf5f474
eglandinselmani <eglandinselmani1@gmail.com>
2025-04-22 20:48:38 +0200

    day12part2

---
+++ Day12Parttwo/signup.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link rel="stylesheet" href="main.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+  </head>
+  <body>
+
+  <div class="login">
+    <form action="" class="form-signin">
+    <h1 class="h3 mb-3 front-weight-normal">Please sign in</h1>
+
+    <label for="inputEmail" class="sr-only">Name</label>
+    <input type="text" id="inputEmail" class="form-control" placeholder="password" name="username" require autofocus>
+
+
+
+    <label for="inputEmail" class="sr-only">Username</label>
+    <input type="text" id="inputEmail" class="form-control" placeholder="password" name="username" require autofocus>
+
+    <label for="inputEmail" class="sr-only">Email</label>
+    <input type="text" id="inputEmail" class="form-control" placeholder="password" name="username" require autofocus>
+
+    <label for="inputEmail" class="sr-only">Password</label>
+    <input type="text" id="inputEmail" class="form-control" placeholder="password" name="username" require autofocus>
+
+    <button class="btn btn-lg btn-primary btn-block mt-3">Sign up</button>
+
+
+
+
+    <small>Already have an account? <a href="signup.php"> Log in`x</a></small>
+    <p class="mt-5 mb-3 text muted">Digital school &copy; 2025</p>
+
+    </form>
+  </div>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous"></script>
+  </body>
+</html>
