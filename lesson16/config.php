9bab3c31a6458cd3d15e887bd0adcf379f94f66c
eglandinselmani <eglandinselmani1@gmail.com>
2025-05-13 20:45:10 +0200

    lesspn16

---
+++ lesson16/config.php
@@ -0,0 +1,16 @@
+<?php
+    
+    $user = "root";
+    $pass = "";
+    $server="localhost";
+    $dbname="dbeglandini";
+
+    try{
+        $conn = new PDO("mysql:host=$server;dbname=$dbname" ,$user,$pass);
+    }catch(PDOException $e){
+        echo"error: ". $e->getMessage();
+    }
+
+
+
+?>
