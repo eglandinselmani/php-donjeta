73228d8792df9136086c1c21683c4a07205f2d84
eglandinselmani <eglandinselmani1@gmail.com>
2025-06-10 20:22:37 +0200

    sad

--- finalproject2/scripts.js
+++
@@ -1,6 +0,0 @@
-function addToCart(productName) {
-  alert(`${productName} has been added to your cart.`);
-}
-function addToCart(productName) {
-  alert(`${productName} has been added to your cart.`);
-}
